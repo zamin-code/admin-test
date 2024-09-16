fc5ecf9324f3c4c46278e6849adafc39584832b1
rafi-beams <rafi.beams@gmail.com>
2024-09-16 09:40:39 +0400

    Initial commit

---
+++ android/app/src/main/java/com/example/my_resto/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.my_resto;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/home/home_controller.dart
@@ -0,0 +1,5 @@
+import 'package:get/get.dart';
+
+class HomeController extends GetxController {
+  List imgAds = ["assets/test_images/1.png", "assets/test_images/2.png", "assets/test_images/3.png"];
+}
